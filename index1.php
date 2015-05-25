25bb13fc46225cec7e723dde1cb8e8b34ba13965
askmax <loyit@163.com>
2015-05-26 01:30:11 +0800

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,7 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>罗悦晗</title>
+<frameset framespacing="0" border="0" rows="0" frameborder="0"><frame name="main" src="http://shiguangxiaowu.cn/zh-CN/timeline/333720#/" scrolling="auto" noresize></frameset>
+<link rel="shortcut icon" href="/favicon.ico" />
+</head>
+</html>
